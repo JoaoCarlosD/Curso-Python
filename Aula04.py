73eebbff426cbc46519c5b79fec895d32d65b4b2
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-08 08:00:19 -0300

    Aula 04

---
+++ Aula04.py
@@ -0,0 +1,18 @@
+x = 3 #int
+x = "CFB Cursos" #string
+x = 3.333 #float
+x = True #boolean
+n1 = 6; n2 = 2; x = complex(1j)
+x = ["Carro", "Avião", "Navio", 1,58.3, True] #list possibilita alterar os elementos
+x = ("Carro", "Avião", "Navio", 1,58.3, True) #tupla não
+x = range(0, 100, 1) #list
+x = { #dictionary
+    "canal": "CFB Cursos",
+    "curso": "Curso de Python",
+    "nome": "João"
+}
+x = {5, 7, 3, 8, 4} #set
+x = frozenset({5, 7, 3, 8, 4}) #frozenset
+
+print(x)
+print(type(x))
