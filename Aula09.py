39581294483af6d3b9a9e6ea67261f07f7f2e2a7
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-08 11:23:34 -0300

    Aula 09

---
+++ Aula09.py
@@ -0,0 +1,13 @@
+a = 6
+b = 3
+res = 0
+op = "/"
+if op == "+":
+    res = a+b
+if op == "-":
+    res = a-b
+if op == "*":
+    res = a*b
+if op == "/":
+    res = a / b
+print(str(a) + op + str(b) + " = " + str(res))
