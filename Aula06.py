0949ef7586a5964dc19af83255b913f29ee9ea9b
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-08 08:58:12 -0300

    Aula 06

---
+++ Aula06.py
@@ -0,0 +1,10 @@
+cidade = "Domingos Martins"
+dia = 10
+mes = "Julho"
+ano = 2019
+canal = "CFB Cursos"
+data = "{}, {} de {} de {}\n \"{}\""
+
+#\' \" \n \r \t \b
+
+print(data.format(cidade, dia, mes, ano, canal))
