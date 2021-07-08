3804e1f089102b7478bb58a693ed29d55be7ea05
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-08 08:18:44 -0300

    Aula 05

---
+++ Aula05.py
@@ -0,0 +1,19 @@
+import random
+
+num_1 = 3
+num_f = 5.7
+num_c = 1j
+num_r = [ #list
+    random.randrange(0, 59),
+    random.randrange(0, 59),
+    random.randrange(0, 59),
+    random.randrange(0, 59),
+    random.randrange(0, 59),
+    random.randrange(0, 59),
+]
+print("Valor 1: " + str(num_r[0]))
+print("Valor 2: " + str(num_r[1]))
+print("Valor 3: " + str(num_r[2]))
+print("Valor 4: " + str(num_r[3]))
+print("Valor 5: " + str(num_r[4]))
+print("Valor 6: " + str(num_r[5]))
