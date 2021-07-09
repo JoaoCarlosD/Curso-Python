34ec18e6d490a15652e91aa816727b7f2eaba849
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-09 14:32:12 -0300

    Aula 12

---
+++ Aula12.py
@@ -0,0 +1,8 @@
+import os
+from time import sleep
+os.system("cls") #Limpa o console
+n1 = int(input("Digite o primeiro valor..: "))
+n2 = int(input("Digite o primeiro valor...: "))
+soma = n1 + n2
+print(f"Soma dos valores: {soma}")
+sleep(10)
