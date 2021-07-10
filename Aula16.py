125e8624dddcca30ea717b8d4345344fc2b2df6c
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-07-09 22:03:16 -0300

    Aula 16

---
+++ Aula16.py
@@ -0,0 +1,19 @@
+#Chave/Key - Valor/Value
+Carro1 = {
+    "Fabricante":"Honda",
+    "Modelo":"HRV"
+}
+Carro2 = {
+    "Fabricante":"Volkswagem",
+    "Modelo":"Golf"
+}
+Carro3 = {
+    "Fabricante":"Volkswagem",
+    "Modelo":"Golf"
+}
+Carros = {
+    "C1":Carro1,
+    "C2":Carro2,
+    "C3":Carro3
+}
+print(Carros["C1"])
