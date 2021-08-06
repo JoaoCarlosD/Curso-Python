249ab3c6b44ef17d05c05b8b4da7f6ad8dc64781
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-08-06 14:55:23 -0300

    Aulas 34 e 35

--- Aula35.py
+++ Aula35.py
@@ -1,4 +1,15 @@
 from tkinter import *
+import os
+
+c = os.path.dirname(__file__)
+nomeArquivo = c+"\\nome.txt"
+def gravarDados():
+    arquivo = open(nomeArquivo,"a")
+    arquivo.write(f"Nome....: {vnome.get()}")
+    arquivo.write(f"\nTelefone: {vfone.get()}")
+    arquivo.write(f"\nEmail...: {vemail.get()}")
+    arquivo.write("\nOBS.....:%s" % vobs.get("1.0", END))
+    arquivo.close()
 
 app = Tk()
 app.title("CFB Cursos")
@@ -21,4 +32,6 @@ vemail.place(x=10,y=130,width=300,height=20)
 Label(app,text="OBS",background="#dde",foreground="#009",anchor=W).place(x=10,y=160,width=100,height=20)
 vobs = Text(app)
 vobs.place(x=10,y=180,width=300,height=80)
+
+Button(app,text="Gravar dados",command=gravarDados).place(x=10,y=270,width=100,height=20)
 app.mainloop()
