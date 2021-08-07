6eaec2d570dea5c0d7c69ee9b86280ac06aff4c2
JoaoCarlosD <joaocarlosdioscanio@gmail.com>
2021-08-07 15:00:19 -0300

    Aulas 38 e 39

--- Aula38.py
+++ Aula38.py
@@ -1,34 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
 
-def mostrarMsg(tipomsg,msg):
-    if tipomsg == "1":
-        messagebox.showinfo(title="CFB Cursos",message=vmsg)
-    elif tipomsg == "2":
-        messagebox.showwarning(title="CFB Cursos",message=msg)
-    elif tipomsg == "3":
-        messagebox.showerror(title="CFB Cursos",message=msg)
-def resetarTB():
-    res = messagebox.askyesno("Resetar","Confirma reset do textobox?")
+app = Tk()
+app.title("Aula 64")
+app.geometry("500x300")
+
+def showMessage(tipo, msg):
     # askyesno / askquestion - SIM e NÃo (True e False)
     # askokcancel - OK e CANCELAR (True e False)
     # askretrycancel - REPETIR e CANCELAR (True e False)
     # askyesnocancel - SIM, NÃO e CANCELAR (True, False e None)
-    if res == True:
-        tb_num.delete(0,END)
-        tb_num.insert(0,"1")
-vmsg = "Curso de Python / Tkinter"
-app = Tk()
-app.title("CFB Cursos")
-app.geometry("500x300")
+    if tipo == "info":
+        messagebox.showinfo(title="App", message=msg)
+    elif tipo == "warning":
+        messagebox.showwarning(title="App", message=msg)
+    elif tipo == "error":
+        messagebox.showerror(title="Error", message=msg)
+    else:
+        print("Erro")
 
-vnum_cstexto = StringVar()
+lista_options = ["info", "warning", "error"]
 
-Label(app,text="Tipo de cx (1, 2 ou 3)").pack()
-tb_num = Entry(app,textvariable=vnum_cstexto)
-vnum_cstexto.set("1")
-tb_num.pack()
+Label(app, text="Tipo de caixa")
+vtipo = StringVar()
+vtipo.set("Selecione uma opção")
 
-btn_msg = Button(app,text="Mostrar mensagem",command=lambda:mostrarMsg(vnum_cstexto.get())).pack()
+txtTipos = Label(app, text="Tipos de caixa")
+txtTipos.pack()
 
-app.mainloop
+op_tipos = OptionMenu(app, vtipo, *lista_options).pack()
+
+Label(app, text = "Digite a mensagem").pack()
+msg = Entry(app)
+msg.pack()
+
+Button(app, text="Criar caixa", command=lambda:showMessage(vtipo.get(), msg.get())).pack()
+
+app.mainloop()
